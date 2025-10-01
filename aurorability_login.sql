92de80d817ab572a9f84a331afad2ee257c2f014
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2025-10-01 20:42:24 -0300

    ajustandoBanco

--- aurorability_login.sql
+++ aurorability_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12/09/2025 às 01:00
+-- Tempo de geração: 02/10/2025 às 01:41
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -36,6 +36,14 @@ CREATE TABLE `comentarios` (
   `data_criacao` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `servico_id`, `usuario_id`, `titulo_comentario`, `comentario`, `data_criacao`) VALUES
+(4, 15, 9, 'Iniciando', 'Para iniciar o projeto\n1. Navbar\n2. Hero Section\n3. Noticias', '2025-09-15 20:20:51'),
+(5, 15, 9, 'testando de novo', 'aaaaaaaaaaaaa', '2025-09-16 19:12:08');
+
 -- --------------------------------------------------------
 
 --
@@ -48,7 +56,7 @@ CREATE TABLE `servico` (
   `tipo_servico_id` int(11) NOT NULL,
   `data_inicio` date NOT NULL DEFAULT current_timestamp(),
   `data_termino` date NOT NULL,
-  `status` enum('Em Andamento','Concluido','Cancelado','') NOT NULL DEFAULT 'Em Andamento'
+  `status` enum('Em Andamento','Concluído','Cancelado','') NOT NULL DEFAULT 'Em Andamento'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -56,11 +64,9 @@ CREATE TABLE `servico` (
 --
 
 INSERT INTO `servico` (`id`, `usuario_id`, `tipo_servico_id`, `data_inicio`, `data_termino`, `status`) VALUES
-(10, 2, 1, '2025-09-07', '2025-09-11', 'Em Andamento'),
-(11, 6, 2, '2025-09-07', '2025-09-24', 'Em Andamento'),
-(12, 6, 3, '2025-09-07', '2025-09-17', 'Em Andamento'),
-(13, 9, 3, '2025-09-07', '2025-09-09', 'Em Andamento'),
-(14, 9, 2, '2025-09-07', '2025-09-12', 'Em Andamento');
+(11, 6, 2, '2025-09-07', '2025-09-24', 'Concluído'),
+(12, 6, 3, '2025-09-07', '2025-09-17', 'Concluído'),
+(15, 9, 3, '2025-09-15', '2025-09-25', 'Em Andamento');
 
 -- --------------------------------------------------------
 
@@ -99,7 +105,7 @@ CREATE TABLE `usuarios` (
   `cpf` varchar(14) NOT NULL,
   `rg` varchar(20) NOT NULL,
   `genero` varchar(50) NOT NULL,
-  `numero` varchar(11) NOT NULL,
+  `numero` varchar(15) NOT NULL,
   `caminho_foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -108,12 +114,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `nivel`, `data_criacao`, `cpf`, `rg`, `genero`, `numero`, `caminho_foto`) VALUES
-(2, 'Nickolas', 'nickolascremasco@gmail.com', '$2y$12$SUIniei8BHTtiiDHQZaPHuh.yDfv7T/lqwd8eRjDsLkorWmcpnaYm', 1, '2025-06-29', '', '', '', '', NULL),
+(2, 'Nick', 'nickolascremasco@gmail.com', '$2y$10$cYZR4o0QG.gwlLLK6TwmF.68tSut4dyZyOMgvxdpUGIvsjBO6yaAe', 1, '2025-06-29', '', '', '', '', NULL),
 (4, 'Nayara', 'nay@gmail.com', '$2y$12$h.mll7/3FcUzdEEKRCVZ7.YYq.t6TGplLZEU.ns0zeJEIhbbvGBEa', 0, '2025-08-19', '', '', '', '', NULL),
 (6, 'Alexandra Sarandy', 'alexandra@gmail.com', '$2y$10$Qv.s5PD7TDAsDLnCrpjnmeGjjHWEnYOa/x50FbdohqkBC/JtYO5Z6', 0, '2025-09-01', '445.238.438-21', '55.235.435', 'masculino', '', '../img/uploads/68b63c556cf2a-usuarioGenerico.jpg'),
-(7, 'Renato', 'renato@gmail.com', '$2y$10$WJHFVS7GtSoUetWELIIs1ubop7alkGXDPmqllGxnkWdRRaYWLsKzi', 0, '2025-09-04', '555.555.555-55', '77-777.777', 'masculino', '', NULL),
-(8, 'numeroFunciona', 'numero@gmail.com', '$2y$10$0acJ7PchcLegLQ1t8c3e0Oprto3i4vZwIt2GAXPO00SHPkXsKEHM.', 0, '2025-09-04', '111.111.111-11', '11-111.111', 'prefiro_nao_dizer', '', NULL),
-(9, 'teste', 'teste@gmail.com', '$2y$10$5TPGdJli8ITIUydfwR2oo.oOzTfikOKVR5r0HZC7wl21gAqYvpuKq', 0, '2025-09-05', '453.453.453-45', '43-534.534', 'masculino', '', NULL);
+(9, 'teste', 'teste@gmail.com', '$2y$10$5TPGdJli8ITIUydfwR2oo.oOzTfikOKVR5r0HZC7wl21gAqYvpuKq', 0, '2025-09-05', '453.453.453-45', '43-534.534', 'masculino', '', NULL),
+(14, 'testeF', 'testandoF@gmail.com', '$2y$10$M8Z2LHfPz0./88dRXx7rFOSuwaVzLsmYa/VlZih9oVRRWwcZoNI6.', 0, '2025-10-01', '445.238.438-21', '55-235.435', 'masculino', '(21) 97455-', '../img/uploads/68ddbb56940a2-4161d2c1db40f36f4c258e6b5561c0ee.jpg');
 
 --
 -- Índices para tabelas despejadas
@@ -124,8 +129,8 @@ INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `nivel`, `data_criacao`,
 --
 ALTER TABLE `comentarios`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_servico_id` (`servico_id`),
-  ADD KEY `fk_usuario_id` (`usuario_id`);
+  ADD KEY `fk_usuario_id` (`usuario_id`),
+  ADD KEY `fk_servico_id` (`servico_id`);
 
 --
 -- Índices de tabela `servico`
@@ -155,13 +160,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `servico`
 --
 ALTER TABLE `servico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `tipos_servico`
@@ -173,7 +178,7 @@ ALTER TABLE `tipos_servico`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restrições para tabelas despejadas
@@ -183,7 +188,7 @@ ALTER TABLE `usuarios`
 -- Restrições para tabelas `comentarios`
 --
 ALTER TABLE `comentarios`
-  ADD CONSTRAINT `fk_servico_id` FOREIGN KEY (`servico_id`) REFERENCES `servico` (`id`),
+  ADD CONSTRAINT `fk_servico_id` FOREIGN KEY (`servico_id`) REFERENCES `servico` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `fk_usuario_id` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
 
 --
